2102e14ef9ac3440f6ffb7cff7d9344cd777df91
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-07-24 16:01:23 -0500

    creando primer endPoint

---
+++ aprendiendo-api-asp-net/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aprendiendo_api_asp_net.Controllers
+{
+    public class ClienteController : ControllerBase
+    {
+        public ClienteController() { }
+
+        [HttpGet]
+        [Route("cliente")]
+        public dynamic listarCliente()
+        {
+            return new {
+                nombre = "Wilgen García",
+            };
+        }
+    }
+}
