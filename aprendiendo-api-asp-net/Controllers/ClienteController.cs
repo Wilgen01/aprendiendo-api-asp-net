a6f8bf2b40e568bf23197305bb8892d9b3dae474
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-07-24 16:25:58 -0500

    metodo post

--- aprendiendo-api-asp-net/Controllers/ClienteController.cs
+++ aprendiendo-api-asp-net/Controllers/ClienteController.cs
@@ -1,18 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using aprendiendo_api_asp_net.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace aprendiendo_api_asp_net.Controllers
 {
+    [ApiController]
+    [Route("cliente")]
     public class ClienteController : ControllerBase
     {
         public ClienteController() { }
 
         [HttpGet]
-        [Route("cliente")]
-        public dynamic listarCliente()
+        [Route("")]
+        public List<Persona> listarCliente()
         {
-            return new {
-                nombre = "Wilgen García",
+            return new List<Persona>{
+                new Persona
+                {
+                    nombres = "Wilgen",
+                    apellidos = "García",
+                    edad = 21
+
+                }
             };
         }
+
+        [HttpPost]
+        [Route("agregar")]
+        public IActionResult agregarCliente(Persona persona)
+        {
+            return Ok(persona);
+        }
+
+
     }
 }
---
+++ aprendiendo-api-asp-net/Models/Persona.cs
@@ -0,0 +1,9 @@
+﻿namespace aprendiendo_api_asp_net.Models
+{
+    public class Persona
+    {
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public int edad { get; set; }
+    }
+}
